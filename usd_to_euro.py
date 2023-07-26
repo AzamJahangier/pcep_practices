da4a7c09f5d9d482613512a8c7501c4b4333f7ce
AzamJahangier <140151775+AzamJahangier@users.noreply.github.com>
2023-07-26 20:07:01 +0200

    Add files via upload
    Euro to USD converter and vice versa

---
+++ usd_to_euro.py
@@ -0,0 +1,8 @@
+﻿usd = 25.89
+euro = 12.0
+
+euro_to_usd = euro * 1.10725
+usd_to_euro = usd * 0.903252
+
+print(euro, "Euro is", round(euro_to_usd, 2), "USD")
+print(usd, "USD is", round(usd_to_euro, 2), "Euro")
